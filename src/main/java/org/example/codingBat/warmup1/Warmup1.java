71b2fe81c88fd22e84f3de52ca5e94325f79bd6e
Sum <msc.mars92@gmail.com>
2026-01-24 21:50:51 +0000

    posNeg

--- src/main/java/org/example/codingBat/warmup1/Warmup1.java
+++ src/main/java/org/example/codingBat/warmup1/Warmup1.java
@@ -24,4 +24,17 @@ public class Warmup1 {
        public boolean parrotTrouble(boolean talking,int hour){
        return (talking && (hour <= 7 || hour > 20));
        }
+
+       public boolean makes10(int a,int b){
+       return (a == 10 || b == 10 || a + b == 10);
+       }
+
+       public boolean posNeg(int a,int b,boolean negative){
+       if (negative){
+           return (a < 0 && b < 0);
+       }else{
+           return ((a < 0 && b > 0) || (a > 0 && b < 0));
+       }
+       }
+
 }
